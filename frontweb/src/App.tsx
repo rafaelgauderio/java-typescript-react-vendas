43606576d5d509cd83fea4eecb8c2cac99fe961b
Rafael de Luca <deluca1712@gmail.com>
2023-03-16 11:25:41 -0300

    primeiro component React

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
 
   return (
+    <>
     <h1>Projeto inicial de dashboard de vendas</h1>
+    <NotificationButton />
+    </>
   )
   
 }
---
+++ frontweb/src/components/NotificationButton/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+function NotificationButton () {
+  return (
+    <p className="notification-paragraph">Primeiro Component</p>
+  )
+}
+
+export default NotificationButton;
