63c00914f8d042b8a39951517b124fc771a0ec76
Rafael de Luca <deluca1712@gmail.com>
2023-03-16 11:03:29 -0300

    Limpando o projeto

--- frontweb/src/main.tsx
+++ frontweb/src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// biblioteca React faz o processo de renderização
+// React processa as informações e apresenta um resultado um tela gráfixa
+// Renderização = processo de transformar código em imagens
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
