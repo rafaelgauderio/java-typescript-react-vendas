3f456706aa399c64e35ddc6efc6560b9c0842ec8
Rafael de Luca <deluca1712@gmail.com>
2023-03-17 12:01:32 -0300

    Configuracao de CORS

---
+++ backend/src/main/java/com/rafaeldeluca/vendas/configuration/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.rafaeldeluca.vendas.configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+// por default um aplicação não pode ter o backend e frontend hospedados em locais diferentes
+// para poder liberar esse acesso é preciso fazer a configuração de cors
+
+@EnableWebSecurity
+@AutoConfiguration
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain filtroDeCadeia(HttpSecurity httpSecurity) throws Exception {
+		
+		try {
+			httpSecurity.cors().and().csrf().disable();
+			httpSecurity.headers().frameOptions().disable();
+			httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+			httpSecurity.authorizeHttpRequests((authentication) -> authentication.anyRequest().permitAll());
+		} catch (Exception erro) {
+			
+			erro.getMessage();
+		}
+		return httpSecurity.build();
+		
+	}
+	
+	@Bean
+	CorsConfigurationSource controladorDeConfiguracaoDeCors () {
+		CorsConfiguration configuracaoDeCors = new CorsConfiguration().applyPermitDefaultValues();
+		List<String> metodosPermitidos = new ArrayList<String>();
+		metodosPermitidos.add("GET");
+		metodosPermitidos.add("POST");
+		metodosPermitidos.add("PUT");
+		metodosPermitidos.add("DELETE");
+		metodosPermitidos.add("OPTIONS");
+		configuracaoDeCors.setAllowedMethods(metodosPermitidos);
+		
+		final UrlBasedCorsConfigurationSource controlador = new UrlBasedCorsConfigurationSource();
+		controlador.registerCorsConfiguration("/**", configuracaoDeCors);
+		
+		return controlador;		
+		
+	}
+	
+
+}
